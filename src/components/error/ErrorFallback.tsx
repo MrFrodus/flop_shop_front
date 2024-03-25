fd17cbe5142f5801fea42853ab4bf8c7fa0f96e3
MrFrodus <gulashiko220@gmail.com>
2024-03-25 14:18:51 +0200

    create error fallback component

--- src/components/error/ErrorFallback.tsx
+++ src/components/error/ErrorFallback.tsx
@@ -1,21 +1,32 @@
 import { FallbackProps } from "react-error-boundary";
+import { useNavigate } from "react-router-dom";
 
 const ErrorFallback = (props: FallbackProps) => {
   const { error, resetErrorBoundary } = props;
 
-  console.log(error);
+  const navigate = useNavigate();
 
   return (
     <div>
-      <div>
+      <div className="flex text-3xl bold mb-2 mt-4 ml-4 flex-col justify-center items-center">
         <h1>Error!!!</h1>
         <p>{error.message}</p>
-        <button
-          type="button"
-          onClick={resetErrorBoundary}
-        >
-          Reload Page
-        </button>
+        <div className="text-sky-500 ">
+          <button
+            type="button"
+            onClick={resetErrorBoundary}
+            className="mx-2 underline"
+          >
+            Reload Page
+          </button>
+          <button
+            type="button"
+            onClick={() => navigate("/")}
+            className="mx-2 underline"
+          >
+            Go to Home Page
+          </button>
+        </div>
       </div>
     </div>
   );
